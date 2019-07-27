1d55b7953d0a5defa6d6b333f8fdaf83f416ae2b
Radhika Guniganti <gunigantirg@gmail.com>
2019-07-27 11:49:49 -0400

    Added URIs for seeing and updating reimbursement requests

--- ERSProject/src/main/java/com/revature/controller/LoginController.java
+++ ERSProject/src/main/java/com/revature/controller/LoginController.java
@@ -7,12 +7,12 @@ import com.revature.model.Employee;
 import com.revature.service.EmployeeService;
 
 public class LoginController implements LoginControllerInterface{
-	
+
 	private static LoginControllerInterface logincontroller = new LoginController();
 	private static final Logger logger = Logger.getLogger(LoginController.class);
-	
+
 	private LoginController() {}
-	
+
 	public static LoginControllerInterface getInstance() {
 		return logincontroller;
 	}
@@ -22,49 +22,48 @@ public class LoginController implements LoginControllerInterface{
 		if(request.getMethod().equals("GET")) {
 			return "Login.html";
 		}
-		
-	Employee loggedIn = EmployeeService.getInstance().getEmployeeInformation(new Employee(0,null,null,null,
-			request.getParameter("username"),request.getParameter("password"),
-			request.getParameter("role")));
-	
-	logger.trace("Logging in");
-	
-	if(loggedIn==null) {
-		logger.warn("Unable to sign in");
-		return "Authentication failed";
-	}
-	
-	logger.info("You are signed in");
-	request.getSession().setAttribute("loggedIn",loggedIn);
-	
-	if(request.getParameter("role").equals("Employee")) {
-		return HomeController.getInstance().EmployeeHome(request);
-	}
-	
-	else if(request.getParameter("role").equals("Manager")) {
-		return HomeController.getInstance().ManagerHome(request);
+
+		Employee loggedIn = EmployeeService.getInstance().getEmployeeInformation(new Employee(0,null,null,null,
+				request.getParameter("username"),request.getParameter("password"),
+				request.getParameter("role")));
+
+		logger.trace("Logging in");
+
+		if(loggedIn==null) {
+			logger.warn("Unable to sign in");
+			return "Authentication failed";
+		}
+
+		logger.info("You are signed in");
+		request.getSession().setAttribute("loggedIn",loggedIn);
+
+		if(request.getParameter("role").equals("Employee")) {
+			return HomeController.getInstance().EmployeeHome(request);
+		}
+
+		else if(request.getParameter("role").equals("Manager")) {
+			return HomeController.getInstance().ManagerHome(request);
+		}
+
+		return null;
 	}
 
-	return null;
-	}
-	
 
 	@Override
 	public String logout(HttpServletRequest request) {
 		// TODO Auto-generated method stub
 		request.getSession().invalidate();
 		return "Login.html";
-		
 	}
 
 	@Override
 	public Object viewAccount(HttpServletRequest request) {
 		// TODO Auto-generated method stub
-		
-	Employee loggedIn = EmployeeService.getInstance().getEmployeeInformation(new Employee(null,
-			null,null,request.getParameter("username"),request.getParameter("password"),
+
+		Employee loggedIn = EmployeeService.getInstance().getEmployeeInformation(new Employee(null,
+				null,null,request.getParameter("username"),request.getParameter("password"),
 				request.getParameter("role")));
-		
+
 		return request.getSession().getAttribute("loggedIn");
 	}
 
--- ERSProject/src/main/java/com/revature/request/RequestHelper.java
+++ ERSProject/src/main/java/com/revature/request/RequestHelper.java
@@ -26,12 +26,30 @@ public class RequestHelper {
 			return HomeController.getInstance().ManagerHome(request);
 		case "/ERSProject/register.do":
 			return RegistrationController.getInstance().register(request);
-		case "/ERSProject/ViewAllEmployees.do":
-			return RegistrationController.getInstance().viewEmployees(request);
-		case "/ERSProject/ViewAccount.do":
+		case "/ERSProject/viewMyAccount.do":
 			return LoginController.getInstance().viewAccount(request);
+		case "/ERSProject/updateAccount.do":
+			return RegistrationController.getInstance().updateInformation(request);
+		case "/ERSProject/viewAllAccounts.do":
+			return RegistrationController.getInstance().viewEmployees(request);
+		case "/ERSProject/updateRequests.do":
+			return RegistrationController.getInstance().updateRequests(request);
+		case "/ERSProject/insertRequest.do":
+			return RegistrationController.getInstance().insertRequests(request);
+		case "/ERSProject/insertRequests.do":
+			return RegistrationController.getInstance().insertRequests(request);
+		case "/ERSProject/lookRequestByID.do":
+			return RegistrationController.getInstance().viewRequestsbyEmployee(request);
+		case "/ERSProject/viewPendingRequests.do":
+			return RegistrationController.getInstance().viewPendingRequests(request);
+		case "/ERSProject/viewResolvedRequests.do":
+			return RegistrationController.getInstance().viewResolvedRequests(request);
+		case "/ERSProject/viewPendingRequestsID.do":
+			return RegistrationController.getInstance().viewPendingRequestsByID(request);
+		case "/ERSProject/viewResolvedRequestsID.do":
+			return RegistrationController.getInstance().viewResolvedRequestsByID(request);
 			
-			
+					
 		default:
 			return "404";
 		
